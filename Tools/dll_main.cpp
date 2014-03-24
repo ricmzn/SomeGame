74d6b3a0e43cc521f0692d9b78c6bb6b18ca3369
Ricardo Maes <ricmzn@gmail.com>
2014-03-24 19:49:04 -0300

    Clean up previous work

--- Tools/dll_main.cpp
+++ Tools/dll_main.cpp
@@ -1,1 +1,13 @@
+#include <QApplication>
+#include "ToolWindow.h"
 
+extern "C"
+{
+    int main(int argc, char** argv)
+    {
+        QApplication app(argc, argv);
+        ToolWindow window;
+        window.show();
+        return app.exec();
+    }
+}
